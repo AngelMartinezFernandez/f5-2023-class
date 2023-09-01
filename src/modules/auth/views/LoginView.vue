cb5602cafc7260e37f745a63f912d356ea596ca9
Angel Martínez <angelmanuelmartinezfernandez@gmail.com>
2023-09-01 22:32:22 +0200

    login inputs added

--- src/modules/auth/views/LoginView.vue
+++ src/modules/auth/views/LoginView.vue
@@ -4,6 +4,10 @@
 
 <template>
 <h1>LoginView</h1>
+    <label for="user">{{ $t('auth.username') }}</label>
+    <input id="user" name="user" type="text">
+    <label for="password">{{ $t('auth.password') }}</label>
+    <input id="password" name="password" type="password">
 </template>
 
 <style scoped>

e366719390fed3a9070f8237435c2fbfdbe91274
Angel Martínez <angelmanuelmartinezfernandez@gmail.com>
2023-09-06 20:18:51 +0200

    LoginView.vue mock approach done

--- src/modules/auth/views/LoginView.vue
+++ src/modules/auth/views/LoginView.vue
@@ -1,13 +1,27 @@
 <script setup>
+import Button from "@/modules/shared/components/Button.vue";
+import router from "@/router";
 
+// methods
+const login = () => {
+    router.push({name: 'products'})
+}
 </script>
 
 <template>
-<h1>LoginView</h1>
-    <label for="user">{{ $t('auth.username') }}</label>
-    <input id="user" name="user" type="text">
-    <label for="password">{{ $t('auth.password') }}</label>
-    <input id="password" name="password" type="password">
+    <div></div>
+    <div>
+        <h1>LoginView</h1>
+        <label for="user">{{ $t('auth.username') }}</label>
+        <input id="user" name="user" type="text">
+        <label for="password">{{ $t('auth.password') }}</label>
+        <input id="password" name="password" type="password">
+        <Button
+            :title="$t('Login')"
+            :type="'action'"
+            @click="login"
+        />
+    </div>
 </template>
 
 <style scoped>
